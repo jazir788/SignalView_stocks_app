4b32b0e1a22ca44c9fc5c66b4e717099c96961d5
jazir788 <jazirsx5@gmail.com>
2025-12-14 11:46:29 +0000

    Added dummy user and dropdown

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,12 @@
+import Header from "@/components/header"
 import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>Click me</Button>
-      
+    <div className="flex min-h-screen home-wrapper">
+      Home
     </div>
+    
   )
 }
 
---
+++ src/components/UserDrowpdown.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useRouter } from "next/navigation"
+
+const UserDrowpdown = () => {
+    const router = useRouter();
+
+    const handleSignOut = async () => {
+        router.push("/sign-in")
+    }
+
+    //Dummy user - test
+    const user = {name: 'test', email: 'test@gmail.com'}
+
+  return (
+    <DropdownMenu>
+  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+  )
+}
+
+export default UserDrowpdown
